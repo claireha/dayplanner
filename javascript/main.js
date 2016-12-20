bd4c511dada0ec35c5e91ac68866ce628af476b3
Claire Ha <clairex3@gmail.com>
2016-12-19 21:11:13 -0500

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -145,27 +145,58 @@ function addPinsToGoogleMap(data) {
 	    }
 	});
 
-
 	// Adds pins to the map and adds map to the page
 	const map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      zoom: 13
     });
 
 	geocode(data['activity-1-location'], function(results){
-		map.setCenter(results[0].geometry.location);
+		let loc1;
+		let loc2;
+
+		let addr1;
+		let addr2;
+
+		loc1 = results[0].geometry.location;
+		addr1 = results[0].formatted_address;
+		map.setCenter(loc1);
         var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
         });
+
+ 		geocode(data['activity-2-location'], function(results){
+ 			loc2 = results[0].geometry.location;
+ 			addr2 = results[0].formatted_address;
+ 			console.log('##########', results)
+			map.setCenter(loc2);
+	        var marker = new google.maps.Marker({
+	            map: map,
+	            position: results[0].geometry.location
+	        });
+			var directionsService = new google.maps.DirectionsService;
+	        var directionsDisplay = new google.maps.DirectionsRenderer({
+	          draggable: true,
+	          map: map,
+	          panel: document.getElementById('right-panel')
+	        });
+
+	        directionsDisplay.addListener('directions_changed', function() {
+	          computeTotalDistance(directionsDisplay.getDirections());
+	        });
+
+	        console.log(addr1, addr2)
+
+	        displayRoute(addr1, addr2, directionsService,
+	            directionsDisplay);
+		});
 	});
-	geocode(data['activity-2-location'], function(results){
-		map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-	});
+
+
+	
+
+	 
 
 }
 
@@ -181,6 +212,58 @@ function geocode(address, callback) {
 		}
 	});
 }
+
+// GOOGLE DRAGGABLE MAP START
+
+function initMap() {
+
+        // var map = new google.maps.Map(document.getElementById('map'), {
+        //   zoom: 4,
+        //   center: {lat: -24.345, lng: 134.46}  // Australia.
+        // });
+
+        // var directionsService = new google.maps.DirectionsService;
+        // var directionsDisplay = new google.maps.DirectionsRenderer({
+        //   draggable: true,
+        //   map: map,
+        //   panel: document.getElementById('right-panel')
+        // });
+
+        // directionsDisplay.addListener('directions_changed', function() {
+        //   computeTotalDistance(directionsDisplay.getDirections());
+        // });
+
+        // displayRoute('Perth, WA', 'Sydney, NSW', directionsService,
+        //     directionsDisplay);
+      }
+
+      function displayRoute(origin, destination, service, display) {
+        service.route({
+          origin: origin,
+          destination: destination,
+          // waypoints: [{location: 'Adelaide, SA'}, {location: 'Broken Hill, NSW'}],
+          travelMode: 'DRIVING',
+          avoidTolls: true
+        }, function(response, status) {
+          if (status === 'OK') {
+            display.setDirections(response);
+          } else {
+            alert('Could not display directions due to: ' + status);
+          }
+        });
+      }
+
+      function computeTotalDistance(result) {
+        var total = 0;
+        var myroute = result.routes[0];
+        for (var i = 0; i < myroute.legs.length; i++) {
+          total += myroute.legs[i].distance.value;
+        }
+        total = total / 1000;
+        document.getElementById('total').innerHTML = total + ' km';
+      }
+
+// GOOGLE END
 
 
 
